79d8616f1a204f33229df2672935d59c1f761c85
PanWor <97786623+PanWor@users.noreply.github.com>
2023-02-20 16:08:42 +0100

    Od teraz komputer nie będzie robił losowych ruchów!
    W skrócie minimax.

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -14,19 +14,90 @@ const winningCombinations = [
   [2, 4, 6]
 ];
 
-function computerTurn() {
-    const cellElements = document.querySelectorAll('[data-cell]');
-    const availableCells = [];
-    cellElements.forEach(cell => {
-      if (!cell.classList.contains('x') && !cell.classList.contains('o')) {
-        availableCells.push(cell);
+function evaluate(board) {
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] = winningCombinations[i];
+    if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+      if (board[a] === 'O') {
+        return 10;
+      } else if (board[a] === 'X') {
+        return -10;
       }
-    });
-    const randomCell = availableCells[Math.floor(Math.random() * availableCells.length)];
-    randomCell.classList.add('o');
-    checkGameStatus();
+    }
+  }
+  return 0;
+}
+
+function areMovesLeft(board) {
+  return board.includes(null);
+}
+
+function minimax(board, depth, isMaximizingPlayer) {
+  const score = evaluate(board);
+  
+  if (score === 10 || score === -10 || !areMovesLeft(board)) {
+    return score - depth;
   }
   
+  if (isMaximizingPlayer) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === null) {
+        board[i] = 'O';
+        bestScore = Math.max(bestScore, minimax(board, depth + 1, false));
+        board[i] = null;
+      }
+    }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === null) {
+        board[i] = 'X';
+        bestScore = Math.min(bestScore, minimax(board, depth + 1, true));
+        board[i] = null;
+      }
+    }
+    return bestScore;
+  }
+}
+
+function computerTurn() {
+  const board = [];
+  cellElements.forEach(cell => {
+    board.push(cell.classList.contains('x') ? 'X' : (cell.classList.contains('o') ? 'O' : null));
+  });
+  
+  let bestScore = -Infinity;
+  let bestMove = null;
+  
+  for (let i = 0; i < 9; i++) {
+    if (board[i] === null) {
+      board[i] = 'O';
+      const score = minimax(board, 0, false);
+      board[i] = null;
+      
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = i;
+      }
+    }
+  }
+  
+  cellElements[bestMove].classList.add('o');
+  checkGameStatus();
+}
 
   function handleCellClick(e) {
     const cell = e.target;
@@ -39,7 +110,7 @@ function computerTurn() {
     cell.classList.add(currentClass);
     checkGameStatus();
   
-    if (gameIsLive) {
+    if (gameIsLive && !xIsNext) {
       statusElement.innerHTML = "It's the computer's turn";
       setTimeout(computerTurn, 1000);
     }
