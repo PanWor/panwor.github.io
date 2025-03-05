4c7f428b6575323b04ecb0f2028be1af9a3ea9a8
PanWor <97786623+PanWor@users.noreply.github.com>
2025-03-05 15:58:17 +0100

    button -> buttons fix (featuredProjects)

--- featuredProjects.js
+++ featuredProjects.js
@@ -114,8 +114,8 @@ fetch(whereIsJSON())
                             ${source ? `<a class="button small flex" href="${source}" target="_blank" rel="noreferrer">${buttons.source} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width:0.75rem;height:0.75rem;fill:currentColor"><g data-name="Layer 2"><g data-name="external-link"><rect width="24" height="24" opacity="0"></rect><path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z"></path><path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z"></path></g></g></svg></a>` : ``}
                             ${seeMore ? `<a class="button small flex" href="${seeMore}" rel="noreferrer">${buttons['see-more']} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width:0.75rem;height:0.75rem;fill:currentColor"><g data-name="Layer 2"><g data-name="external-link"><rect width="24" height="24" opacity="0"></rect><path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z"></path><path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z"></path></g></g></svg></a>` : ``}
                         </div>
-                        ${pcCompatibility ? `<img class="compatibility" src="${imagesSource}pc.svg" alt="${button['pc']}" title="${button['pc']}">` : ``}
-                        ${mobileCompatibility ? `<img class="compatibility mobile" src="${imagesSource}mobile.svg" alt="${button['mobile']}" title="${button['mobile']}">` : ``}
+                        ${pcCompatibility ? `<img class="compatibility" src="${imagesSource}pc.svg" alt="${buttons['pc']}" title="${buttons['pc']}">` : ``}
+                        ${mobileCompatibility ? `<img class="compatibility mobile" src="${imagesSource}mobile.svg" alt="${buttons['mobile']}" title="${buttons['mobile']}">` : ``}
                     </div>
                 `
                 
